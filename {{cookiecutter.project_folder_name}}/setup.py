ba5ae34521dc6a4c07215767910082bb4b9429d9
Sebastien Delisle <sebastien.delisle@broadsign.com>
2018-08-20 09:25:33 -0400

    Add pipenv

--- {{cookiecutter.project_folder_name}}/setup.py
+++ {{cookiecutter.project_folder_name}}/setup.py
@@ -1,6 +1,17 @@
 from setuptools import setup, find_packages
+from pipenv.project import Project
+from pipenv.utils import convert_deps_to_pip
+
+pfile = Project(chdir=False).parsed_pipfile
+requirements = convert_deps_to_pip(pfile['packages'], r=False)
+test_requirements = convert_deps_to_pip(pfile['dev-packages'], r=False)
+
+print(requirements)
+
 setup(
     name="{{cookiecutter.package_name}}",
     version="{{cookiecutter.package_version}}",
     packages=find_packages(exclude=['tests']),
+    install_requires=requirements,
+    tests_require=test_requirements
 )
